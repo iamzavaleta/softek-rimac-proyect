dabbee0fd3c957293ffcc788769aa3200c290d71
iamzavaleta <alexander.zavaleta@unmsm.edu.pe>
2023-10-10 02:39:06 -0500

    [feat] Config & implement swagger UI

---
+++ src/types/people.d.ts
@@ -0,0 +1,10 @@
+export interface People {
+    nombre: string;
+    altura: number;
+    masa: number;
+    color_cabello: string;
+    color_piel: string;
+    color_ojos: string;
+    fecha_nacimiento: string;
+    genero: string;
+}
